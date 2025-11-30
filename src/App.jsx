8a499539a07085a40dd549f25d73616297ab3a73
Hossein Dabire <hosseindaberi@gmail.com>
2025-11-30 17:33:12 +0330

    feat: add basic NoteApp layout structure
    Implement initial layout with header, note form and note list containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import "./App.css";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div className="container">
+      <div className="note-header">
+        <h1>NoteApp</h1>
+      </div>
+      <div className="note-app">
+        <div className="add-new-note">add new note form</div>
+        <div className="note-container">note lists</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
